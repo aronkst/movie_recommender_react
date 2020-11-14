c13b2b55755cde91bdb99341ce63232c6c4c0d1e
Aron <aron.0504@hotmail.com>
2020-11-14 11:43:37 -0300

    create toolbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  button: {
+    marginLeft: '1em',
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            movie_recommender
+          </Typography>
+          <Button className={classes.button} color="inherit">Watched</Button>
+          <Button className={classes.button} color="inherit">Recommended</Button>
+          <Button className={classes.button} color="inherit">Blocked</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
