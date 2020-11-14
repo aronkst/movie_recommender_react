b978464885f99d22d275f451038d2deddfc72b9d
Aron <aron.0504@hotmail.com>
2020-11-14 14:44:47 -0300

    component Menu

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from 'react';
+import Menu from './components/Menu/Menu';
 import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  button: {
-    marginLeft: '1em',
-  }
-}));
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const classes = useStyles();
-
   return (
     <div className="App">
       <Router>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography className={classes.title} variant="h6" noWrap>movie_recommender</Typography>
-            <Button className={classes.button} color="inherit" component={Link} to="/watched">Watched</Button>
-            <Button className={classes.button} color="inherit" component={Link} to="/">Recommended</Button>
-            <Button className={classes.button} color="inherit" component={Link} to="/blocked">Blocked</Button>
-          </Toolbar>
-        </AppBar>
+        <Menu />
         <Container>
-        <Switch>
+          <Switch>
             <Route path="/watched">
               <h1>watched</h1>
             </Route>
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Aux from './../../hoc/Aux/Aux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  button: {
+    marginLeft: '1em',
+  }
+}));
+
+const Menu = (_) => {
+  const classes = useStyles();
+
+  return (
+    <Aux>
+      <AppBar position="static">
+          <Toolbar>
+            <Typography className={classes.title} variant="h6" noWrap>movie_recommender</Typography>
+            <Button className={classes.button} color="inherit" component={Link} to="/watched">Watched</Button>
+            <Button className={classes.button} color="inherit" component={Link} to="/">Recommended</Button>
+            <Button className={classes.button} color="inherit" component={Link} to="/blocked">Blocked</Button>
+          </Toolbar>
+        </AppBar>
+    </Aux>
+  );
+}
+
+export default Menu;
