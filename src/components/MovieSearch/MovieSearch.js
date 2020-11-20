d6e8a9158c02669210418f9982dd426163235c9d
Aron <aron.0504@hotmail.com>
2020-11-19 21:11:56 -0300

    remove reset in search

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Card from './../Card/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
-import ReplayIcon from '@material-ui/icons/Replay';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
 
@@ -12,10 +10,7 @@ const useStyles = makeStyles((_) => ({
   title: {
     marginTop: '0',
   },
-  textfield: {
-    marginTop: '1em',
-  },
-  buttons: {
+  marginTop: {
     marginTop: '1em',
   },
 }));
@@ -38,15 +33,8 @@ const MovieSearch = (_) => {
       <h2 className={classes.title}>ADD WATCHED MOVIE</h2>
       <p>FILL IN THE FIELD BELOW TO SEARCH THE MOVIE THAT YOU WANT TO ADD AS WATCHED.</p>
       <form onSubmit={handleSubmit}>
-        <TextField className={classes.textfield} fullWidth variant="outlined" InputLabelProps={{ shrink: true, }} name="title" label="TITLE" />
-        <Grid container spacing={2} className={classes.buttons}>
-          <Grid item xs={6}>
-            <Button fullWidth variant="contained" color="secondary" startIcon={<ReplayIcon />} type="reset">Reset</Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Button fullWidth variant="contained" color="primary" startIcon={<SearchIcon />}>Search</Button>
-          </Grid>
-        </Grid>
+        <TextField className={classes.marginTop} fullWidth variant="outlined" InputLabelProps={{ shrink: true, }} name="title" label="TITLE" />
+        <Button fullWidth variant="contained" color="primary" startIcon={<SearchIcon />} className={classes.marginTop}>Search</Button>
       </form>
     </Card>
   );
